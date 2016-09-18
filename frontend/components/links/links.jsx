ec94393d596b22a8b53e65eaea7d3442348053c2
Jessica Terry <jessica_terry@alumni.brown.edu>
2016-09-18 11:48:47 -0700

    GitHub caps fix

--- frontend/components/links/links.jsx
+++ frontend/components/links/links.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Links = () => (
   <div className="links">
-    <a href="http://www.github.com/JescaLyn">Github</a>
+    <a href="http://www.github.com/JescaLyn">GitHsub</a>
     <a href="http://www.linkedin.com/in/JescaTerry">LinkedIn</a>
   </div>
 );
